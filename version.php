96f9e491252e4c2f86a6b2509d6ed2613cd069fd
Dan Marsden <dan@danmarsden.com>
2015-08-04 10:02:25 +1200

    Add release/maturity tags.

--- version.php
+++ version.php
@@ -27,3 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version  = 2015073000;
 $plugin->requires = 2015050500;
 $plugin->component = 'quiz_randomsummary';
+$plugin->release = '1';
+$plugin->maturity = MATURITY_STABLE;
